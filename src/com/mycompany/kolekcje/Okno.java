68d3028b5aa9e4d1186aad1fb21deee67cc38039
KrzysztofMatyjaszczykPollub <krzysztof.matyjaszczyk1@pollub.edu.pl>
2020-11-14 15:40:24 +0100

    First step, before task 7.1

---
+++ src/com/mycompany/kolekcje/Okno.java
@@ -0,0 +1,15 @@
+package com.mycompany.kolekcje;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Okno {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Moje okno!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new Panel());
+        frame.setPreferredSize(new Dimension(800, 600));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
